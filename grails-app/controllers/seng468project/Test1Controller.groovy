2a503d327ceb8f0dff4236290ca1372efff225ac
kennethhyk <kennethhyk@gmail.com>
2018-01-06 16:06:10 -0800

    test controller demo

---
+++ grails-app/controllers/seng468project/Test1Controller.groovy
@@ -0,0 +1,9 @@
+package seng468project
+
+class Test1Controller {
+    def testService
+
+    def index() {
+        testService.serviceMethod()
+    }
+}
---
+++ grails-app/domain/seng468project/Test.groovy
@@ -0,0 +1,7 @@
+package seng468project
+
+class Test {
+    String lizheng
+    static constraints = {
+    }
+}
