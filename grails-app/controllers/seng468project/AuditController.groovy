176f49fc85513a8f1c2ea3104ee04533d8405846
kennethhyk <kennethhyk@gmail.com>
2018-03-20 13:38:46 -0700

    i think it is done

--- grails-app/controllers/seng468project/AuditController.groovy
+++ grails-app/controllers/seng468project/AuditController.groovy
@@ -1,11 +1,29 @@
 package seng468project
 
+import groovyx.net.http.HTTPBuilder
+
+import static groovyx.net.http.ContentType.JSON
+import groovyx.net.http.AsyncHTTPBuilder
+
 class AuditController {
 
     def save() {
-        def res = request.JSON
-        LogHistory lh = new LogHistory(res.user as String, res.log as String)
+        String user = request.getParameter("user")
+        String log_msg = request.getParameter("log_msg")
+        LogHistory lh = new LogHistory(user, log_msg)
         lh.save()
         render status:200
+    }
+
+    def hi() {
+        String user = UUID.randomUUID().toString()
+        String log_msg = UUID.randomUUID().toString()
+        def postBody = [user: user, log_msg: log_msg]
+        def http = new AsyncHTTPBuilder(
+                poolSize : 1,
+                uri : 'http://localhost:8080',
+                contentType : JSON )
+        http.post(path: '/audit/save', body: postBody)
+        render status: 200
     }
 }
